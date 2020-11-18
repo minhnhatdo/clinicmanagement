fe7a5c0b1cc706a756df8698dc8411a7fbe32071
minhnhatdo <minhnhat.maicogroup@gmail.com>
2020-11-18 20:39:24 +0700

    init app

---
+++ app/__init__.py
@@ -0,0 +1,32 @@
+from flask import Flask, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin, AdminIndexView
+from flask_login import LoginManager, current_user
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:123456789@localhost/clinic_management?charset=utf8"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['ROOT_PROJECT_PATH'] = app.root_path
+app.secret_key = 'ahiahiahiah'
+
+
+db = SQLAlchemy(app=app)
+
+
+class UserModelView(AdminIndexView):
+    def is_accessible(self):
+        return current_user.is_active and current_user.is_authenticated
+
+    def _handle_view(self, name):
+        if not self.is_accessible():
+            return redirect('/login')
+
+
+admin = Admin(app=app, name="Quản lý phòng mạch", template_mode="bootstrap4", index_view=UserModelView())
+
+
+login = LoginManager(app=app)
+
+
+
---
+++ app/utils.py
@@ -0,0 +1,20 @@
+import json, hashlib
+from app.models import User
+from app import db
+
+
+def add_user(email, username, password, avatar):
+    password = str(hashlib.md5(password.encode('utf-8')).hexdigest())
+    u = User(
+             email=email,
+             username=username,
+             password=password,
+             avatar=avatar)
+    try:
+        db.session.add(u)
+        db.session.commit()
+
+        return True
+    except Exception as ex:
+        print(ex)
+        return False
